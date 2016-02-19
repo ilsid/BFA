14eff273614b12f337a5bc92cc154d452ce13d70
ilsid <ilsidv@gmail.com>
2016-02-19 15:56:11 +0200

    SDK introduction

---
+++ src/main/sdk/java/com/ilsid/bfa/sdk/ActionBuilder.java
@@ -0,0 +1,16 @@
+package com.ilsid.bfa.sdk;
+
+/**
+ * Builds custom action.
+ * 
+ * @author illia.sydorovych
+ *
+ */
+public class ActionBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("ActionBuilder was run");
+
+	}
+
+}

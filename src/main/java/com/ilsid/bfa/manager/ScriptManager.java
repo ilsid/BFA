1b7279403f3e7d23b5c2383d4729af40e762ae27
ilsid <ilsidv@gmail.com>
2016-04-20 17:12:14 +0300

    Code cleanup

--- src/main/java/com/ilsid/bfa/manager/ScriptManager.java
+++ src/main/java/com/ilsid/bfa/manager/ScriptManager.java
@@ -2,8 +2,6 @@ package com.ilsid.bfa.manager;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
